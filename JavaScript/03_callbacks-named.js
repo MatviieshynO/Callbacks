bb032d1371a2537547d568176cfbca2771e35366
MatviieshynO <Olopromotion@gmail.com>
2024-03-06 00:57:46 +0100

    examples bind and closure, named callbacks

---
+++ JavaScript/03_callbacks-named.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fs = require('node:fs');
+
+{
+    const print = (file, err, data) => {
+        console.log({ file, lines: data.split('\n').length });
+    };
+
+    const fileName = './1-callback.js';
+
+    const callback = print.bind(null, fileName);
+    fs.readFile(fileName, 'utf8', callback);
+}
+
+// Callback with closure
+
+{
+    const print = (file) => (err, data) => {
+        console.log({ file, lines: data.split('\n').length });
+    };
+
+    const fileName = './1-callback.js';
+
+    const callback = print(fileName);
+    fs.readFile(fileName, 'utf8', callback);
+}
